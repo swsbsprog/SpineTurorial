1d6a1565cd060e777a4d7b1963685a7a80b66f3e
hktrollgames <hktrollgames@gmail.com>
2022-10-23 15:35:00 +0900

    선택된 캐릭터 커서 표시

---
+++ Assets/Hero.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        print(name);
+        TrunManager.instance.SetHero(this);
+    }
+}
---
+++ Assets/TrunManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrunManager : MonoBehaviour
+{
+    public static TrunManager instance;
+    public Transform cursorTr;
+    void Start()
+    {
+        instance = this;
+    }
+
+    internal void SetHero(Hero hero)
+    {
+        //hero 선택되었다는 알림 표시하자.
+        cursorTr.gameObject.SetActive(true);
+        cursorTr.position = hero.transform.position;
+
+        //hero와 관련된 메뉴 표시하자. 
+    }
+}
