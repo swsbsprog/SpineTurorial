c250d5320e0ed7d4c431515de45b33248359a647
hktrollgames <hktrollgames@gmail.com>
2022-10-23 15:48:28 +0900

    메뉴 연결

--- Assets/TrunManager.cs
+++ Assets/TrunManager.cs
@@ -7,17 +7,25 @@ public class TrunManager : MonoBehaviour
 {
     public static TrunManager instance;
     public Transform cursorTr;
+    Hero _hero;
     void Start()
     {
         instance = this;
+
     }
 
     internal void SetHero(Hero hero)
     {
+        _hero = hero;
         //hero 선택되었다는 알림 표시하자.
         cursorTr.gameObject.SetActive(true);
         cursorTr.position = hero.transform.position;
 
         //hero와 관련된 메뉴 표시하자. 
+
+    }
+    public void PassHeroTurn()
+    {
+        print($"{_hero}의 턴을 종료한다");
     }
 }
