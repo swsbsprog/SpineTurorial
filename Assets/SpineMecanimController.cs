f675f1cdb8778f5e10faa8bc2bef719ed9e78dd0
hktrollgames <hktrollgames@gmail.com>
2022-10-23 14:28:08 +0900

    기본 이동

---
+++ Assets/SpineMecanimController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Spine.Unity;
+public class SpineMecanimController : MonoBehaviour
+{
+    public Animator animator;
+    public SkeletonMecanim skeletonMecanim;
+    private float speed = 2f;
+    void Update()
+    {
+        Vector2 move = new Vector2(0, 0); //Vector2.zero
+        if (Input.GetKey(KeyCode.W)) move.y = 1; // 위
+        if (Input.GetKey(KeyCode.S)) move.y = -1; // 아래
+        if (Input.GetKey(KeyCode.A)) move.x = -1; // 좌
+        if (Input.GetKey(KeyCode.D)) move.x = 1; // 우
+        move.Normalize(); // 길이 1로 만들기
+        animator.SetFloat("speed", move.magnitude);
+        transform.Translate(speed * Time.deltaTime * move);
+        FlipX(move.x);
+    }
+
+    private void FlipX(float x) // 
+    {
+        if (x == 0)
+            return;
+        skeletonMecanim.Skeleton.ScaleX = x > 0 ? 1 : -1;
+    }
+}
--- Assets/SpineTest.cs
+++ Assets/SpineTest.cs
@@ -7,11 +7,23 @@ public class SpineTest : MonoBehaviour
 {
     [SpineAnimation]
     public string animationName;
+    [SpineAnimation]
+    public string addAnimationName;
     public int track = 0;
+    public float scaleX = 1;
+
+    SkeletonAnimation sa;
     void Start()
     {
-        SkeletonAnimation sa = GetComponent<SkeletonAnimation>();
-        sa.AnimationState.SetAnimation(track, animationName, true);
-        //sa.AnimationState.SetAnimation(track, "blink", true);
+        sa = GetComponent<SkeletonAnimation>();
+        sa.AnimationState.SetAnimation(track, animationName, false);
+        sa.AnimationState.AddAnimation(track, addAnimationName, true, 0);
+        //UpdateScale(sa);
+    }
+
+    [ContextMenu("UpdateScale")]
+    private void UpdateScale()
+    {
+        sa.Skeleton.ScaleX = scaleX;
     }
 }
